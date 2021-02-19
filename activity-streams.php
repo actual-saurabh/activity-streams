1be1b04effbad9244c4b0f357e6c7e7564a7fe23
Parth Gohil <parth.gohil@gmail.com>
2021-02-19 10:44:26 +0530

    adds the main php file and the necessary doc block to it

---
+++ activity-streams.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Activity Streams
+ *
+ * @package           Bodh
+ * @author            Saurabh, Parth
+ * @copyright         none
+ * @license           MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Activity Streams
+ * Plugin URI:        https://github.com/actual-saurabh/activity-streams
+ * Description:       Records the activity of your e learning coursez.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4.3
+ * Author:            Saurabh, Parth
+ * Author URI:        https://github.com/actual-saurabh, https://github.com/parth-gohil
+ * Text Domain:       activity-streams
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ */
