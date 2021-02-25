f59f6011e74661bd41379a939fce13602806351e
Parth Gohil <parth.gohil@gmail.com>
2021-02-25 14:38:09 +0530

    registers custom post types

--- activity-streams.php
+++ activity-streams.php
@@ -20,3 +20,34 @@
  * License:           MIT
  * License URI:       https://opensource.org/licenses/MIT
  */
+
+add_action( 'wp_loaded', 'register_custom_posts', 50, 0 );
+
+/**
+ * Method register_custom_posts
+ *
+ * @return void
+ */
+function register_custom_posts() {
+	$args_step = array(
+		'label'	       => 'Step',
+		'description'  => 'Smallest achiavable item in Path',
+		'show_ui'      => true,
+		'show_in_menu' => true,
+	);
+	$args_path = array(
+		'label'	       => 'Path',
+		'description'  => 'Smallest achiavable item in Plan',
+		'show_ui'      => true,
+		'show_in_menu' => true,
+	);
+	$args_plan = array(
+		'label'	       => 'Plan',
+		'description'  => 'largest Planned item',
+		'show_ui'      => true,
+		'show_in_menu' => true,
+	);
+	register_post_type( 'step', $args_step );
+	register_post_type( 'path', $args_path );
+	register_post_type( 'plan', $args_plan );
+}
